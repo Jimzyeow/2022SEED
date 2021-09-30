8b99c2a2684197cbbc6573b447f993a58579c2f8
gsheng19 <litekirax3@gmail.com>
2021-09-30 12:57:44 +0800

    added cart routing to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 
 import LoginPage from "./components/Login/Login";
+import Cart from './components/Cart/Cart';
+
 
 function App() {
   return (
