dcddca1688c302e6461163a8a700880530afaaa7
Lee Ee Hong <eehong.kyle@gmail.com>
2021-09-30 11:51:46 +0800

    updated LoginPage with email and password field validation before sending to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 import LoginPage from "./components/Login/Login";
 
 function App() {
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,15 +1,80 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
+
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    //add all validation here , only if true then isValid return true
+    //currently only must have "@"" sign.
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    //add all validation here, only if true, isValid return true
+    //currently only must be longer than 8 character.
+    return { value: action.val, isValid: action.val.trim().length > 8 };
+  }
+  return { value: "", isValid: false };
+};
 
 function LoginPage() {
+  //state to check if overall form is vali.
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  // useReducer to dispatch
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  //extract out isValid from emailState and pwState
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
+  // useEffect to check form validity everytime inputs change/
+  useEffect(() => {
+    console.log("Checking form validity!");
+    setFormIsValid(emailIsValid && passwordIsValid);
+  }, [emailIsValid, passwordIsValid]);
+
+  //function to handle typing into email field
+  const emailHandler = (event) => {
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  //function to handle typing into pw field
+  const passwordHandler = (event) => {
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    //for now just console logging to make sure everything works.
+    console.log(emailState.isValid);
+    console.log(passwordState.isValid);
+    console.log(formIsValid);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div>
         <label htmlFor="email">Your Email</label>
-        <input type="email" id="email" required />
+        <input onChange={emailHandler} type="email" id="email" required />
       </div>
       <div>
         <label htmlFor="password">Your Password</label>
-        <input type="password" id="password" required />
+        <input
+          onChange={passwordHandler}
+          type="password"
+          id="password"
+          required
+        />
       </div>
       <div>
         <button>Login</button>
