5530ac9490eddc58e651be597d6cbc77e14c1349
blaizmannen <76086010+blaizmannen@users.noreply.github.com>
2021-09-30 14:38:46 +0800

    add to cart in Product Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ViewProducts from './components/Product/ViewProducts';
 import ProductDetails from './components/Product/ProductDetails';
+import ViewCategories from './components/Product/ViewCategories';
 function App() {
   return (
     <Router>
       <div className="App">
           <Switch>
             <Route exact path="/">
+              <ViewCategories />
+            </Route>
+            <Route path="/:id/products/">
               <ViewProducts />
             </Route>
             <Route path="/products/:id">
